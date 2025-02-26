989155845b2601ef54cbb3dda9125dff851c0c8c
dbestdev <wilson@dbestdev.com>
2025-02-26 03:34:29 +0100

    Initiliazed Firebase

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,14 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color orangeColor = Color(0xFFFF9933);
+  static const Color blueColor = Color(0xFF3366CC);
+  static const Color redColor = Color(0xFFFF6666);
+  static const Color purpleColor = Color(0xFFCC3399);
+  static const Color background = Color(0xFFFFFFFF);
+}
+
+/*Blue color - 3366cc
+Orange Color - ff9933
+Red Color - ff6666
+Purple Color - cc3399*/
