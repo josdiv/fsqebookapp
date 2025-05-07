b887c0c7b23e6f387b091a4b5f20467e1bc6fd5d
dbestdev <wilson@dbestdev.com>
2025-05-07 12:10:09 +0100

    Updated search icons in Latest page

--- lib/features/latest/presentation/screens/widgets/latest_header_widget.dart
+++ lib/features/latest/presentation/screens/widgets/latest_header_widget.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 
 import '../../../../../core/theme/app_colors.dart';
+import '../../../../home/presentation/screens/search_books_page/search_books_page.dart';
 
 class LatestHeaderWidget extends StatelessWidget {
   const LatestHeaderWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
+    void navigateToSearch(BuildContext context) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (_) => const SearchBooksPage()),
+      );
+    }
+
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
@@ -18,7 +26,10 @@ class LatestHeaderWidget extends StatelessWidget {
             color: AppColors.blueColor,
           ),
         ),
-        Icon(Icons.search)
+        GestureDetector(
+          onTap: () => navigateToSearch(context),
+          child: Icon(Icons.search),
+        )
       ],
     );
   }
